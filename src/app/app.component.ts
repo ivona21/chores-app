b81009dcb0f903fb0585e3d096bf3c951c555a0e
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-01-22 17:10:20 +0100

    header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = "Love your home";
+  subtitle: string = "every day";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { CustomMaterialModule } from './custom-material.module';
 
 
 @NgModule({
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]

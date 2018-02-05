16d0932d5dd46113aa2ea799969dca6a02fbd8ac
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-02-05 13:59:02 +0100

    always get chore from all chores and not from the server to simplify two way binding

--- src/app/chores/chores.service.ts
+++ src/app/chores/chores.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import { Subject } from "rxjs/Subject";
+import { Subscription } from "rxjs/Subscription";
 import { Observable } from "rxjs/Observable";
 import { Chore } from "./chore.model";
 
@@ -11,10 +12,11 @@ export class ChoresService {
     gotChore = new Subject<Chore>();
     url: string = "https://love-your-home.firebaseio.com/";
     chores: Chore[] = [];
+    choresChangesSubscription: Subscription;
 
     constructor(private http: Http) { }
 
-    getChores() : void {
+    getChores(): void {
         this.http.get(this.url + "chores.json").subscribe(
             (response: Response) => {
                 let responseBody = JSON.parse(response["_body"]);
@@ -32,52 +34,67 @@ export class ChoresService {
         )
     }
 
+    private getChoreFromChores(id: string) {
+        let found: Chore = this.chores.find((chore) => {
+            return chore.id === id;
+        });
+        this.gotChore.next(found);
+    }
+
     getById(id: string): void {
-        let found : Chore;
+        //always get chore from all chores and not from the server to simplify two way binding
         if (this.chores.length > 0) {
-            found = this.chores.find((chore) => {
-                return chore.id === id;          
-            });              
-            this.gotChore.next(found);       
-        } else {
-        this.http.get(this.url + "chores" + "/" + id + ".json").subscribe(
-            (response: Response) => {
-                let responseBody = JSON.parse(response["_body"]);
-                let chore = responseBody ? new Chore(responseBody.id, responseBody.name, responseBody.frequency, new Date(responseBody.lastTime)) : new Chore("", "", 0, new Date());
-                this.gotChore.next(chore);
+            if (this.choresChangesSubscription) {
+                this.choresChangesSubscription.unsubscribe();
             }
-        )}       
-      
+            this.getChoreFromChores(id);
+        } else {
+            this.choresChangesSubscription = this.choresChanged.subscribe(
+                (chores: Chore[]) => {
+                    this.getChoreFromChores(id);
+                }
+            )
+        }
+        //get chore from server
+        // else {
+        // this.http.get(this.url + "chores" + "/" + id + ".json").subscribe(
+        //     (response: Response) => {
+        //         let responseBody = JSON.parse(response["_body"]);
+        //         let chore = responseBody ? new Chore(responseBody.id, responseBody.name, responseBody.frequency, new Date(responseBody.lastTime)) : new Chore("", "", 0, new Date());
+        //         this.gotChore.next(chore);
+        //     }
+        // )}       
+
     }
 
     addChore(choreToInsert: Chore) {
         this.http.post(this.url + "chores.json", choreToInsert).subscribe(
             (response: Response) => {
-                let key = JSON.parse(response["_body"]).name;               
-                let insertedChore: Chore = new Chore(key, choreToInsert.name, choreToInsert.frequency, choreToInsert.lastTime);               
+                let key = JSON.parse(response["_body"]).name;
+                let insertedChore: Chore = new Chore(key, choreToInsert.name, choreToInsert.frequency, choreToInsert.lastTime);
                 this.chores.push(insertedChore);
                 this.choresChanged.next(this.chores.slice());
             }
         )
     }
 
-    updateChore(chore: Chore) {        
-        let index = this.chores.findIndex(c => c.id === chore.id);  
-        let updateUrl = this.url + "chores/" + chore.id + ".json";   
-        let choreToUpdate : Chore = new Chore(chore.id, chore.name, chore.frequency, new Date(chore.lastTime));  
+    updateChore(chore: Chore) {
+        let index = this.chores.findIndex(c => c.id === chore.id);
+        let updateUrl = this.url + "chores/" + chore.id + ".json";
+        let choreToUpdate: Chore = new Chore(chore.id, chore.name, chore.frequency, new Date(chore.lastTime));
         this.http.put(updateUrl, choreToUpdate).subscribe(
             (response: Response) => {
                 let responseBody = JSON.parse(response["_body"]);
-                let updatedChore : Chore = new Chore(
+                let updatedChore: Chore = new Chore(
                     responseBody.id, responseBody.name, responseBody.frequency, new Date(responseBody.lastTime)
                 );
-               this.chores[index] = updatedChore;
-               this.choresChanged.next(this.chores.slice());
+                this.chores[index] = updatedChore;
+                this.choresChanged.next(this.chores.slice());
             }
-        )       
+        )
     }
 
-    deleteChore(choreToDelete: Chore){
+    deleteChore(choreToDelete: Chore) {
         let index = this.chores.findIndex(chore => chore.id === choreToDelete.id);
         this.http.delete(this.url + "chores/" + choreToDelete.id + ".json").subscribe(
             (response: Response) => {
